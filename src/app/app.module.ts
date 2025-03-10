8083f333e2fdbfbb38c5a96822f468d403af46bd
Rafael Vilches <rvilches38@yahoo.com.mx>
2025-03-09 23:56:56 -0600

    fix version and home routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CookieService } from 'ngx-cookie-service';
+
+@NgModule({
+  declarations: [ //TODO: Declaraciones, componentes, directivas, pipes
+    AppComponent,
+  ],
+  imports: [ //TODO: Solo se importan otros modules
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    CookieService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
